5415b15bca9403fd14fa03fd5ca051d4e2019219
Szabolcs Sandor <szabolcs.sandor@gmail.com>
2025-03-12 17:11:25 +0100

    day 17 par 5 update

---
+++ MODULE1/Section_7/practice/test.py
@@ -0,0 +1,11 @@
+user_entries = ['10', '19.1', '20']
+numbers = [float(user_entry) for user_entry in user_entries]
+print(numbers)
+
+numbers = [10, 20, 30]
+results = [number * 2 for number in numbers]
+print(results)
+
+user_entries = ['10', '19.1', '20']
+numbers = [float(user_entry) for user_entry in user_entries]
+print(sum(numbers))
