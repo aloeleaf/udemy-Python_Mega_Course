11db4cf235d9717d9f82b8b68a1968853658b77d
aloeleaf <szabolcs.sandor@gmail.com>
2025-03-17 15:18:22 +0100

    day 19 finished

--- MODULE1/Section19_webapp/web.py
+++ MODULE1/Section19_webapp/web.py
@@ -1,24 +1,32 @@
 import streamlit as st
 from modules import functions
 
-todos = functions.get_todos()
-st.set_page_config(layout='wide')
-
 
 def add_todo():
     # st.session_state output is a dictionary data and "new_todo" is the key
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
-    # st.rerun(scope='app')
+    #st.rerun(scope='app')   
+    
 
+
+todos = functions.get_todos()
 
 # create fix html elements in streamlit
 st.title('My Todo App')
 st.subheader('This is my todo app.')
-st.write(
-    "This app is to increase your <b>productivity</b>.",
-    unsafe_allow_html=True)
+st.write("This app is to increase your productivity")
+
+# create dynamic html elements in streamlit
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        #print(index, "-", todo)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(
     label="New todo:",
@@ -27,12 +35,9 @@ st.text_input(
     key='new_todo',
     label_visibility='hidden')
 
-# create dynamic html elements in streamlit
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        # print(index, "-", todo)
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
+print("End of code")
+
+st.session_state
+
+# st.checkbox("Buy grocery")
+# st.checkbox("Pay bills")
