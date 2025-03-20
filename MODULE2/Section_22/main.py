567232f7564b662d46966ad44039e0a82dcab1b8
aloeleaf <szabolcs.sandor@gmail.com>
2025-03-20 15:31:40 +0100

    day22 add description and links

--- MODULE2/Section_22/main.py
+++ MODULE2/Section_22/main.py
@@ -10,8 +10,8 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.header("Ardit Sulce" )
-    content= """
+    st.header("Ardit Sulce")
+    content = """
     I am a software engineer with a passion for data science and machine learning. I have experience in building web applications, data pipelines, and machine learning models. I am always looking for new challenges and opportunities to learn and grow."""
     st.info(content)
 
@@ -19,13 +19,17 @@ content2 = """Bellow you ca find some of the apps I have buit in Python. Fell fr
 st.write(content2)
 
 
-
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 content = pd.read_csv("data.csv", sep=";")
-with col3:    
+with col3:
     for index, row in content[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 with col4:
     for index, row in content[10:].iterrows():
         st.header(row["title"])
-  
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
