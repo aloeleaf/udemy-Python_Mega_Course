e92843d715beaaaf74572df88a1db5cc00f96f68
aloeleaf <szabolcs.sandor@gmail.com>
2025-03-20 15:20:51 +0100

    day22 render app add titles

--- MODULE2/Section_22/main.py
+++ MODULE2/Section_22/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +10,7 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.write("Ardit Sulce" )
+    st.header("Ardit Sulce" )
     content= """
     I am a software engineer with a passion for data science and machine learning. I have experience in building web applications, data pipelines, and machine learning models. I am always looking for new challenges and opportunities to learn and grow."""
     st.info(content)
@@ -18,3 +19,13 @@ content2 = """Bellow you ca find some of the apps I have buit in Python. Fell fr
 st.write(content2)
 
 
+
+col3, col4 = st.columns(2)
+content = pd.read_csv("data.csv", sep=";")
+with col3:    
+    for index, row in content[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in content[10:].iterrows():
+        st.header(row["title"])
+  
