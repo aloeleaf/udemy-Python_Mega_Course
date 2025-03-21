59d57e0d2c60449bd8aa969a197f68d59d74d5bb
aloeleaf <szabolcs.sandor@gmail.com>
2025-03-21 15:19:50 +0100

    day 24  221.

--- MODULE2/Section_23/pages/Contact_Us.py
+++ MODULE2/Section_23/pages/Contact_Us.py
@@ -1,13 +1,23 @@
 import streamlit as st
+from send_email import send_mail
+
 
 st.header("Contuct Me")
 
 with st.form(key="contact_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
-    submit_button = st.form_submit_button()
+    raw_message = st.text_area("Your message")
+    message = f"""\
+    Subject: New email from {user_email}
+    
+    from: {user_email}    
+    {raw_message}
+    """
+    submit_button = st.form_submit_button("Submit")
     if submit_button:
-        print("pressed")
+        send_mail(message)
+        st.info("Your email was sent successfully")
+        
     
     
     
--- MODULE2/Section_23/send_email.py
+++ MODULE2/Section_23/send_email.py
@@ -1,24 +1,26 @@
 import smtplib, ssl
 
-host = "mail.bkt.birosag.hu"
-port = 25  
-#port = 465  
-
-# username = "szabolcs.sandor@gmail.com"
-# password = "bdwnhqycxpaiihdj"
-
-username = "helpdesk"
-password = "gj/29BF37."
-
-receiver = "sandorsz@birosag.hu"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi there
-This message is sent from Python.
-"""
+def send_mail(message):
+    host = "mail.bkt.birosag.hu"
+    port = 25  
 
 
-with smtplib.SMTP_SSL(host, port, ) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message )
+    # username = "szabolcs.sandor@gmail.com"
+    # password = "bdwnhqycxpaiihdj"
+
+    username = "helpdesk"
+    password = "gj/29BF37."
+
+    receiver = "sandorsz@birosag.hu"
+    #context = ssl.create_default_context()
+
+
+    
+    with smtplib.SMTP(host, port) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message )
+
+    ## SSL connection
+    # with smtplib.SMTP_SSL(host, port) as server:
+    #     server.login(username, password)
+    #     server.sendmail(username, receiver, message )
